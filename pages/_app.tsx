0821801ef79ae16fa78b74543d12b4e737a7de50
hualde <javhualde@gmail.com>
2024-10-08 19:39:48 +0200

    v000

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { UserProvider } from '@auth0/nextjs-auth0/client'
+import { ErrorBoundary } from '@auth0/nextjs-auth0/client'
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <ErrorBoundary>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </ErrorBoundary>
   )
 }
+
+export default MyApp

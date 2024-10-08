cec837bb31685302a41137761d015fe987ad4cdf
hualde <javhualde@gmail.com>
2024-10-08 19:43:46 +0200

    v0000

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { UserProvider } from '@auth0/nextjs-auth0/client'
-import { ErrorBoundary } from '@auth0/nextjs-auth0/client'
+import Auth0ErrorBoundary from '../components/Auth0ErrorBoundary'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ErrorBoundary>
+    <Auth0ErrorBoundary>
       <UserProvider>
         <Component {...pageProps} />
       </UserProvider>
-    </ErrorBoundary>
+    </Auth0ErrorBoundary>
   )
 }
 
